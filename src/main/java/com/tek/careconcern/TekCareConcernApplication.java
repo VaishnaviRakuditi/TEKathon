fb520594cdb41d66ad72334cd7544d076181c60e
SetuDengre <Setu.Dengre@bestwestern.com>
2020-12-09 18:10:23 +0530

    Hello World

---
+++ src/main/java/com/tek/careconcern/TekCareConcernApplication.java
@@ -0,0 +1,15 @@
+package com.tek.careconcern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TekCareConcernApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TekCareConcernApplication.class, args);
+		
+		System.out.println("its working");
+	}
+
+}
---
+++ src/test/java/com/tek/careconcern/TekCareConcernApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tek.careconcern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TekCareConcernApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
